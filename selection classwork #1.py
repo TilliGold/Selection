3f214bf42a182c5d19f4b0cfda8dd1b6894505fe
Tilli Gold <40558@longroad.ac.uk>
2014-10-06 12:02:01 +0100

    selection work

---
+++ selection classwork #1.py
@@ -0,0 +1,12 @@
+#Tilli Gold
+#06-10-2014
+#Selection class exercises
+
+number1 = int(input("Please enter a number: "))
+number2 = int(input("Please enter a second number: "))
+
+if number1==number2:
+    print("The numbers you have entered are the same")
+else:
+    print("The numbers you have entered are different")
+    
---
+++ selection classwork #2.py
@@ -0,0 +1,12 @@
+#Tilli Gold
+#06-10-2014
+#Selection class exercise 2
+
+age = int(input("Please enter your age: "))
+
+if age >=17:
+    print("You are old enough to drive")
+
+else:
+    print("You are not old enough to drive")
+    
